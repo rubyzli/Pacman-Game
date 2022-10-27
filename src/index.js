44a830011697d31c6b95b637dd2a5c29a15e07c6
Jessica <jr.simon07@gmail.com>
2022-10-27 17:10:52 +0200

    movement through divs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector('#game');
 const scoreboard = document.querySelector('#scoreboard');
 
+let width = 28;
+
+
 let bgImg = [
     '../assets/images/ghosts/ghost-blinky-down.png',
     '../assets/images/ghosts/ghost-blinky-left.png',
@@ -8,7 +11,7 @@ let bgImg = [
     '../assets/images/ghosts/ghost-blinky-up.png'
 ]
 
-setInterval('blinkyGhost()', 400);
+// setInterval('blinkyGhost()', 400);
 
 function blinkyGhost() {
     let x = 0;
@@ -20,58 +23,74 @@ function blinkyGhost() {
 }
 
 const layout = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 3, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 3, 1,
-    1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 1,
-    1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1,
-    1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
-    1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1,
-    8, 8, 8, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 8, 8, 8,
-    1, 1, 1, 1, 2, 1, 2, 1, 1, 4, 1, 1, 2, 1, 2, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 1, 5, 2, 6, 1, 2, 2, 2, 2, 2, 2, 2,
-    1, 1, 1, 1, 2, 1, 2, 1, 1, 7, 1, 1, 2, 1, 2, 1, 1, 1, 1,
-    8, 8, 8, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 8, 8, 8,
-    1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1,
-    1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1,
-    1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1,
-    1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1,
-    1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1,
-    1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1,
-    1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1,
-    1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 3, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1],
+    [8, 8, 8, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 8, 8, 8],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 4, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 1, 5, 2, 6, 1, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 7, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [8, 8, 8, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 8, 8, 8],
+    [1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1],
+    [1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1],
+    [1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1],
+    [1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
 const squares = [];
 
 // CreateBoard
-
+let classes = {
+    1: 'wall',
+    2: 'pellet',
+    3: 'powerpellet',
+    4: 'blinkyghost',
+    5: 'pinkyghost',
+    6: 'inkyghost',
+    7: 'clydeghost',
+    8: 'pacman'
+}
+let startPosition = {
+    i: 0,
+    j: 0
+}
 function createBoard () {
     for(let i = 0; i < layout.length; i++){
-        let square = document.createElement('div');
-        canvas.appendChild(square);
-         squares.push(square);
+        for(let j =0; j < layout[i].length; j++){
+            let square = document.createElement('div');
 
-         if(layout[i] === 1){
-            squares[i].classList.add('wall');
-         } else if(layout[i] === 2){
-            squares[i].classList.add('pellet');
-         } else if(layout[i] === 3){
-            squares[i].classList.add('powerpellet');
-         } else if(layout[i] === 4){
-            squares[i].classList.add('blinkyghost');
-         } else if(layout[i] === 5){
-            squares[i].classList.add('pinkyghost');
-         } else if(layout[i] === 6){
-            squares[i].classList.add('inkyghost');
-         } else if(layout[i] === 7){
-            squares[i].classList.add('clydeghost');
-         } else if(layout[i] === 0){
-            squares[i].classList.add('pacman');
-         } 
+            canvas.appendChild(square);
+            // squares[i] = [];
+            squares[`${i}-${j}`] = square;
+            // console.log(squares)
+
+            Object.keys(classes).forEach((x) => {
+                console.log('x', x, 'layout', layout[i][j]);
+                if (layout[i][j] == x) {
+                    console.log("HAHAHAHAH")
+                    squares[`${i}-${j}`].classList.add(classes[x]);
+                }
+            })
+            if(layout[i][j] === 0){
+                startPosition.i = i;
+                startPosition.j = j;
+                squares[`${i}-${j}`].classList.add('pacman');
+                squares[`${i}-${j}`].appendChild(document.querySelector('#pac1'));
+            }
+
     }
 }
+}
+
 
 createBoard();
 
@@ -92,7 +111,7 @@ function update() {
 
 //Pacman movement & Animation
 
-let pacMan = document.getElementById('pac1');
+let pacMan = document.querySelector('#pac1');
 
 
 let images = new Array();
@@ -103,47 +122,96 @@ images = ['./assets/images/pacman_32px/pacman-0.png',
         
 let x = 0;
 
-        setInterval(function animation(){
-            pacMan.src = images[x];
-            x++;
-            if(images.length === x){
-                x = 0;
-            }
-            
-        }, 100);
+setInterval(function animation(){
+    pacMan.src = images[x];
+    x++;
+    if(images.length === x){
+        x = 0;
+    }
+    
+}, 100);
+
+//  function pacmanPosition(){
+//      let pacMan = document.querySelector('#pac1');
+//      let position = pacMan.getBoundingClientRect();
+//      return position;
+//  }
+// let pacPosition = new Object();
+//  pacPosition = pacmanPosition();
+
+//  function wallPosition(wall){
+//     let position = wall.getBoundingClientRect();
+//     return position   
+// }
+// let wallPositionn = new Object();
+// wallPositionn = wallPosition(wall);
+
+
+// function isColliding(position, wallp){
+//     if(
+//         position.x > wallp.x &&
+//         position.y < wallp.y 
+//     ) {
+//         console.log('true')
+//     } else {
+//         console.log('false');
+//     }
+// }
+// setInterval(isColliding(pacPosition, wallPositionn), 20);
+
 pacMan = null;
 
 let animate;
 
 function init(){
-    pacMan = document.getElementById('pac1');
-    pacMan.style.position = 'relative';
+    pacMan = document.querySelector('#pac1');
+    /*pacMan.style.position = 'relative';
     pacMan.style.left = '288px';
-    pacMan.style.top = '96px';
+    pacMan.style.top = '96px';*/
 }
 
+function move(position, direction){
+    // if()
+}
 
 function moveRight(){
     pacMan.style.transform = 'rotate(0deg)';
-    pacMan.style.left = parseInt(pacMan.style.left) + 3 + 'px';
-    animate = setTimeout(moveRight, 20);
+    // pacMan.style.left = parseInt(pacMan.style.left) + 3 + 'px';
+    // animate = setTimeout(moveRight, 20);
+    let pacmanPosition = squares[`${startPosition.i}-${startPosition.j}`];
+    let nextElement = squares[`${startPosition.i}-${startPosition.j+1}`];
+    console.log(nextElement.classList);
+
+    if(nextElement.classList.value == 'pellet'){
+        nextElement.classList.remove('pellet');
+        nextElement.classList.add('pacman');
+        nextElement.appendChild(document.getElementById('pac1'));
+
+        pacmanPosition.empty();
+
+        startPosition.j += 1;
+    }
 }
+
 
 function moveLeft(){
     pacMan.style.transform = 'rotate(180deg)';
     pacMan.style.left = parseInt(pacMan.style.left) - 3 + 'px';
     animate = setTimeout(moveLeft, 20);
 }
+    
 function moveDown(){
     pacMan.style.transform = 'rotate(90deg)';
     pacMan.style.top = parseInt(pacMan.style.top) + 3 + 'px';
-   animate = setTimeout(moveDown, 20);
+    animate = setTimeout(moveDown, 20);
 }
+    
 function moveUp(){
     pacMan.style.transform = 'rotate(-90deg)';
     pacMan.style.top = parseInt(pacMan.style.top) - 3 + 'px'
     animate = setTimeout(moveUp, 20);
 }
+    
 
 function stop(){
     clearTimeout(animate)
@@ -165,6 +233,6 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
-console.log(getCoordinates(pacMan));
 window.onload = init;
+
 
